9a0c6e692a1407cae2f7545477c149d8b896a882
michanagatlajaswanth <150347475+michanagatlajaswanth@users.noreply.github.com>
2025-08-09 10:20:48 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://184.72.122.226:9090/springapp1';
+const BASE_URL = 'http://100.29.13.216:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -46,15 +46,15 @@ const BASE_URL = 'http://184.72.122.226:9090/springapp1';
     setIsEditing(true);
   };
 
- /*
+ 
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
-      await axios.delete(`http://localhost:9090/delete/${id}`);
+      await axios.delete(`http://100.29.13.216:9090/springapp1/delete/${id}`);
       alert('Delete successful');
       fetchProducts();
     }
   };
-  */
+  
 
   useEffect(() => {
     fetchProducts();
@@ -167,14 +167,14 @@ const BASE_URL = 'http://184.72.122.226:9090/springapp1';
                 >
                   Edit
                 </button>
-                {/*
+                {
                 <button
                   className="btn btn-danger btn-sm"
                   onClick={() => deleteProduct(p.id)}
                 >
                   Delete
                 </button>
-                */}
+                }
               </td>
             </tr>
           ))}
